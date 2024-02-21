66521284ad61fa779be4083ac636d148121251f2
Veljko03 <veljkodovragovic@gmail.com>
2024-02-21 18:09:15 +0100

    change grid button, functionality for del curr grid left

--- javaScript.js
+++ javaScript.js
@@ -1,7 +1,9 @@
 
 const container = document.getElementById("container");
 const restart = document.getElementById("restart");
-let choose=64;
+const size = document.getElementById("size");
+let choose= 16;
+console.log(choose);
 
 function make(rows){
     for(i=1; i<=rows; i++){
@@ -10,16 +12,43 @@ function make(rows){
         for(j=1; j<=rows; j++){
             const c = document.createElement("div");
             
+            c.setAttribute("class", "cell")
+            r.appendChild(c);
             
-            r.appendChild(c).className="cell";
-            
-            c.addEventListener("mouseover",()=>{
-                c.style.backgroundColor = "black";
-            });
-            
+             c.addEventListener("mouseover",()=>{
+                 c.style.backgroundColor = "black";
+           });
+           
         }
     }
+    
 }
+
+
+console.log(choose);
+
+
+
+size.addEventListener("click", ()=>{
+    
+        
+    let gridSize = prompt("Choose grid size between 16-64:");
+    if(gridSize !=null && gridSize >0 && gridSize<=64){
+        
+        const cell = document.getElementsByClassName("cell");
+        for(div of cell){
+            div.style.backgroundColor="white";
+        }
+       
+
+        choose=gridSize;
+        console.log(choose);
+        make(choose);
+    } else{
+        alert("wrong input")
+    }
+})
+
 
 restart.addEventListener("click", ()=>{
     window.location.reload();
@@ -28,7 +57,7 @@ restart.addEventListener("click", ()=>{
 
 make(choose);
 
-
+console.log(choose);
 
 
 
