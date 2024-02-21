fedd1c0a3a4ed9e20049eeebca6ee04a5980cf68
Veljko03 <veljkodovragovic@gmail.com>
2024-02-21 14:58:17 +0100

    added div change color to black on hover

--- javaScript.js
+++ javaScript.js
@@ -10,27 +10,18 @@ function make(rows){
             const c = document.createElement("div");
             c.innerText=j;
             r.appendChild(c).className="cell";
+            
+            c.addEventListener("mouseover",()=>{
+                c.style.backgroundColor = "black";
+            });
+            
         }
     }
 }
 make(16);
 
 
-// function createBoxes(numberPerRow) {
-//     const container = document.getElementById("container");   
-//      const total = (numberPerRow * numberPerRow) + numberPerRow;
-//     const mod = numberPerRow + 1;
-//     for (let i = 1; i < total; i++) {
-//       const div = document.createElement('div');
-//       if (i % mod === 0) {
-//         div.style.cssText = "border: 0; height: 0; width: 100%";
-//       } else {
-//         div.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
-//       }
-//       container.appendChild(div);
-//     }
-//   }
-//   createBoxes(16);
+
 
 
 
